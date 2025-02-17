a9dcbb77b7367da902c46bf0acd415c5418f3dfa
Pontus Sjöstedt <po7853sj-s@student.lu.se>
2025-02-17 16:06:16 +0100

    change tab name

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -86,7 +86,7 @@
 </script>
 
 <svelte:head>
-  <title>{$page.data.meta?.title || 'Web'} - Immich</title>
+  <title>{$page.data.meta?.title || 'Web'} - Arkiv</title>
   <link rel="manifest" href="/manifest.json" crossorigin="use-credentials" />
   <meta name="theme-color" content="currentColor" />
   <AppleHeader />
